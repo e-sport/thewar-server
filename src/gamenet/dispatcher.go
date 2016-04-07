6ce9efea0b1341cf53fe77a06bdc114cf52c586d
lightlfyan <lightlfyan@gmail.com>
2016-04-07 18:55:08 +0800

    update

--- src/gamenet/dispatcher.go
+++ src/gamenet/dispatcher.go
@@ -1,10 +1,10 @@
 package gamenet
 
-import "log"
 import (
 	"../api"
 	"../gameproto"
 	"github.com/golang/protobuf/proto"
+	"log"
 )
 
 type handler func(msg proto.Message) (error, []byte)
@@ -16,15 +16,30 @@ var apiMap map[int]handler = map[int]handler{
 	1004: api.HandMethod,
 }
 
+func getPbBody(id int) proto.Message {
+	switch id {
+	case 1001:
+		return &gameproto.Move{}
+	case 1002:
+		return &gameproto.Move{}
+	case 1003:
+		return &gameproto.Move{}
+	case 1004:
+		return &gameproto.Move{}
+	default:
+		return &gameproto.Move{}
+	}
+}
+
 func dispatch(id int, msg []byte, conn *connection) error {
-	pb := &gameproto.Move{}
+	pb := getPbBody(id)
 	err := proto.Unmarshal(msg, pb)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	f, has := apiMap[id]
-	if has {
+	f, ok := apiMap[id]
+	if ok {
 		if err, b := f(pb); err == nil {
 			conn.pushMsg(b)
 		}
