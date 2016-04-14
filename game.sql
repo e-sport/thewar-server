9138a2ab4318b212cd381207a07cf6161889fbb0
lightlfyan <lightlfyan@gmail.com>
2016-04-14 17:29:56 +0800

    update

--- game.sql
+++ game.sql
@@ -18,9 +18,9 @@ SET PASSWORD FOR 'dev'@'localhost' = PASSWORD('dev');
 
 use game1;
 
-CREATE TABLE Users(
-   uid INT NOT NULL,
-   name VARCHAR(100) NOT NULL,
-   password VARCHAR(40) NOT NULL,
-   PRIMARY KEY (uid)
-);
+CREATE TABLE IF NOT EXISTS `users` (
+   `uid` VARCHAR(40) PRIMARY KEY NOT NULL,
+   `user_name` VARCHAR(30) NOT NULL,
+   `password` VARCHAR(44) NOT NULL,
+   `created_at` CHAR(64) NOT NULL,
+   `updated_at` CHAR(64) NOT NULL)
--- src/game/db/online.go
+++ src/game/db/online.go
@@ -15,15 +15,9 @@ type User struct {
 }
 */
 
-type player struct {
-	Uid      int64 `xorm:"primarykey"`
-	Name     string
-	Password string `xorm:"varchar(200)"`
-}
+var _online = map[int]*Users{}
 
-var _online = map[int]*player{}
-
-var engine xorm.Engine
+var engine *xorm.Engine
 var max int
 
 type Users struct {
@@ -36,32 +30,15 @@ type Users struct {
 }
 
 func init() {
-	/*
-		//db, err := sql.Open("mysql", "user:password@tcp(ip:port)/database")
-		_db, err := sql.Open("mysql", "dev:dev@tcp(127.0.0.1:3306)/game1")
-		if err != nil {
-			log.Fatalf("Open database error: %s\n", err)
-		}
-		db = _db
-
-		row := db.QueryRow("select max(uid) from users")
-		row.Scan(&max)
-	*/
-
-	engine, _ := xorm.NewEngine("mysql", "dev:dev@tcp(127.0.0.1:3306)/game1")
+	engine1, _ := xorm.NewEngine("mysql", "dev:dev@tcp(127.0.0.1:3306)/game1")
+	engine = engine1
 	engine.ShowSQL(true)
-
-	/*
-		rows := engine.DB().QueryRow("select max(Uid) from player")
-		rows.Scan(&max)
-	*/
 
 	one := GetUser(1)
 	log.Print(one.UserName)
 
 	/*
 		engine.Sync2(new(Users))
-
 		engine.Insert(&Users{Uid: 1, UserName: "player1", Password: "1"})
 		engine.Insert(&Users{Uid: 2, UserName: "player2", Password: "1"})
 		engine.Insert(&Users{Uid: 3, UserName: "player3", Password: "1"})
@@ -75,7 +52,7 @@ func insert(name string, password string) {
 
 func GetUser(uid int) *Users {
 	user := &Users{}
-	_, err := engine.Where("Uid =?", uid).Get(user)
+	_, err := engine.Where("uid = ?", uid).Get(user)
 	if err != nil {
 		log.Print(err)
 	}
